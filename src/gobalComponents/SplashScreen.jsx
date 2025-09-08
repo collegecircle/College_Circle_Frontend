3b704ca20d39de47259d3a1d543c50a0357c844d
collegecircle <collegecircle.setup@gmail.com>
2025-09-08 16:40:30 +0530

    updated the splash screen

--- src/gobalComponents/SplashScreen.jsx
+++ src/gobalComponents/SplashScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import icon from "/assets/cclogo.PNG";
+import icon from "/assets/icon.jpg";
 
 const Splash = () => {
     const [phase, setPhase] = useState("initial");
